7b9bb0de91a729a374537beda3894b388e05ca65
ada.dobrovinsky@gorkapstroy.ru <dobrowins@gmail.com>
2018-06-19 15:26:55 +0300

    submodule added

--- app/src/main/java/dobrowins/com/habrmultimoduleexample/MainActivity.kt
+++ app/src/main/java/dobrowins/com/habrmultimoduleexample/MainActivity.kt
@@ -2,11 +2,22 @@ package dobrowins.com.habrmultimoduleexample
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import dobrowins.com.randomproviderlibrary.RandomNumberProvider
+import kotlinx.android.synthetic.main.activity_main.*
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var randomNumberProvider: RandomNumberProvider
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val randomNumber = randomNumberProvider.provideRandomNumber(1024)
+        val message = String.format(getString(R.string.initial_message), randomNumber)
+        tvMainActivityMessage.text = message
     }
+
 }
--- app/src/main/java/dobrowins/com/habrmultimoduleexample/di/LibrariesModule.kt
+++ app/src/main/java/dobrowins/com/habrmultimoduleexample/di/LibrariesModule.kt
@@ -1,7 +1,17 @@
 package dobrowins.com.habrmultimoduleexample.di
 
 import dagger.Module
+import dagger.Provides
+import dobrowins.com.randomproviderlibrary.RandomNumberProvider
+import dobrowins.com.randomproviderlibrary.RandomNumberProviderImpl
+import javax.inject.Singleton
 
 @Module
 class LibrariesModule {
+
+    @Provides
+    @Singleton
+    fun randomNumberProvider(): RandomNumberProvider =
+        RandomNumberProviderImpl()
+
 }
